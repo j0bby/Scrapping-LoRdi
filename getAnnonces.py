583de607956f7fbb8c2acfe6a53f6f47d0abe7be
j0bby <mathieuvirsolvy@gmail.com>
2016-07-21 11:53:45 +0200

    add getAnnonces.py
    fonction pour récupérer les annonces, en devenir

---
+++ getAnnonces.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup #parse html
+
+import requests # GET
+
+import re  #regex
+
+url = "https://www.leboncoin.fr/informatique/offres/languedoc_roussillon/?th=1&q="
+
+requete = ["lordi","hp%20pavillon%20x360"]
+
+for i in requete:
+    
+    print("-"*100)
+    print("URL : " + url+i)
+    
+    r= requests.get(url+i)
+    data = r.text
+    
+    # parse html
+    soup = BeautifulSoup(data,"lxml") 
+    
+    # obtenir la section des annonces
+    blockAnnonces = soup.find('section',{'class':'tabsContent block-white dontSwitch'})
+    
+    # obtenir les annonces
+    annonces = blockAnnonces.find_all('li')
+    print('nb annonces : ' + str(len(annonces)))
+    
+    # obtenir le titre de l'annonce
+    titres= [ x.a['title'] for x in annonces]
+    print("les titres :" + str(titres))
+    
+    # obtenir l'url de l'annonce
+    urls= [ x.a['href'] for x in annonces]
+    print(urls)
+    
