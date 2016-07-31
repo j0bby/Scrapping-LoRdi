d644b9ae5ec81a15e1a0279c7fbd978d1a01d7f2
j0bby <mathieuvirsolvy@gmail.com>
2016-07-31 11:35:10 +0200

    correction pour fonds rouges
    utiliser la teinte

--- code/testMathieu.py
+++ code/testMathieu.py
@@ -41,22 +41,33 @@ def findCapot(image):
 
 
 
-image = ndimage.imread( "..\\Exemple-annonces\\ordibidonb.jpg")
+image = ndimage.imread( "..\\Exemple-annonces\\ordi5.jpg")
 
 lower = np.array([70, 2, 2]) #RGB
 upper = np.array([250, 65, 65]) #RGB
 red = cv2.inRange(image, lower, upper)
-
 nbRed=sum(sum(red ==255))
 freqRed=nbRed/(red.shape[1]*red.shape[0])
 print(freqRed)
 
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)   
+lower = np.array([115, 135, 135]) #RGB
+upper = np.array([125, 255, 255]) #RGB
+redH = cv2.inRange(hsv, lower, upper)
+nbRedH=sum(sum(redH ==255))
+freqRedH=nbRedH/(redH.shape[1]*redH.shape[0])
+print(freqRedH)
+
+
 if freqRed >0.3 :
-    print("Red computer")
-    startX,startY,endX,endY = findCapot(image)
-    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
+    if freqRedH<0.3:
+        print("not lordi, check background")
+    else :
+        print("Red computer")
+        startX,startY,endX,endY = findCapot(image)
+        cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
 elif freqRed>0.05 :
     print("housse region")
 elif freqRed>0.02:
