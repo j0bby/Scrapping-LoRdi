fdbb71f570a2c4cc0e578b6ae7c8f78810557d14
j0bby <mathieuvirsolvy@gmail.com>
2016-07-31 11:00:33 +0200

    new new try
    détermine si un ordi rouge est présent sur la photo. Si oui, trouve la
    zone délimité par le capot

---
+++ code/testMathieu.py
@@ -0,0 +1,69 @@
+import numpy as np
+import cv2
+from scipy import ndimage
+import matplotlib.pyplot as plt
+
+def findCapot(image):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    
+    lower = np.array([115, 135, 135]) #RGB
+    upper = np.array([125, 255, 255]) #RGB
+    red = cv2.inRange(hsv, lower, upper)
+    
+    densityL=sum(red==255)/red.shape[1]
+    print(densityL)
+    startX=0
+    while densityL[startX]<0.2:
+        startX+=1
+    print(startX)
+    
+    endX=-1
+    while densityL[endX]<0.2:
+        endX-=1
+    endX+=len(densityL)
+    print(endX)
+    
+    
+    Tred = np.transpose(red)
+    densityC=sum(Tred==255)/Tred.shape[0]
+    print(densityC)
+    startY =0
+    while densityC[startY]<0.2:
+        startY+=1
+    print(startY)
+    
+    endY =-1
+    while densityC[endY]<0.2:
+        endY-=1
+    endY+=len(densityC)
+    print(endY)
+    return startX,startY,endX,endY
+
+
+
+image = ndimage.imread( "..\\Exemple-annonces\\ordi3.jpg")
+
+lower = np.array([70, 2, 2]) #RGB
+upper = np.array([250, 65, 65]) #RGB
+red = cv2.inRange(image, lower, upper)
+
+nbRed=sum(sum(red ==255))
+freqRed=nbRed/(red.shape[1]*red.shape[0])
+print(freqRed)
+
+if freqRed >0.3 :
+    print("Red computer")
+    startX,startY,endX,endY = findCapot(image)
+    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+elif freqRed>0.05 :
+    print("housse region")
+elif freqRed>0.02:
+    print("some Red")
+else:
+    print("not ordi")
+
+
+
+
