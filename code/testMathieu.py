cee9b6061f8b2f39b72d7cfa097ba13767c37217
j0bby <mathieuvirsolvy@gmail.com>
2016-09-26 12:53:47 +0200

    nettoyage

--- code/testMathieu.py
+++ code/testMathieu.py
@@ -5,23 +5,25 @@ import matplotlib.pyplot as plt
 import math
 import imutils
 
-def adjust_gamma(image, gamma=1.0):
-    invGamma = 1.0 / gamma
-    table = np.array([((i / 255.0) ** invGamma) * 255
-    for i in np.arange(0, 256)]).astype("uint8")
-    return cv2.LUT(image, table)
-
 def findCapot(image,freqRed):
+# renvoie la rotation de l'image, et le rectangle contenant l'ordinateur
     maxRed=0
-    good=-1
+    bestAngle=-1
     red = rotateImage(image, 0)
     nbRed=sum(sum(red ==255))
     freqRed=nbRed/(red.shape[1]*red.shape[0])
+    
+    
     for angle in range(0,95,5):
+        
+        #rotation de l'image
         red = rotateImage(image, angle)
+        
+        #délimitation des zones
         densityL=sum(red==255)/red.shape[0]
         startX=0
         freqtemp=freqRed
+        
         while densityL[startX]<freqtemp:
             startX+=1
             if startX==len(densityL)-1:
@@ -37,7 +39,6 @@ def findCapot(image,freqRed):
                 endX=-1
                 freqtemp*=0.9
         endX+=len(densityL)
-
         
         
         Tred = np.transpose(red)
@@ -61,18 +62,22 @@ def findCapot(image,freqRed):
                 freqtemp*=0.9
         endY+=len(densityC)
 
-        
-
         temp= red[startY:endY,startX:endX]
         nbRed= sum(sum(temp==255))
+        #freq=nbRed/(temp.shape[1]*temp.shape[0])
+        
+        
+        #récupération du meilleur résultat (basée sur le nombre de rouge récupérés dans la zone)
         if nbRed>maxRed : 
             RstartX,RstartY,RendX,RendY=startX,startY,endX,endY
             maxRed=nbRed
-            good = angle
-    print(good) 
-    return RstartX,RstartY,RendX,RendY, good
+            bestAngle = angle
+            
+    print(bestAngle) 
+    return RstartX,RstartY,RendX,RendY, bestAngle
     
 def rotateImage(src, angle,scale=1.):
+# rotation de l'image, et redimentionne aux dimensions max 
     w = src.shape[1]
     h = src.shape[0]
     # now calculate new image width and height
@@ -90,77 +95,37 @@ def rotateImage(src, angle,scale=1.):
     return cv2.warpAffine(src, rot_mat, (int(math.ceil(nw)), int(math.ceil(nh))), flags=cv2.INTER_LANCZOS4)
 
 def locateLogo(image):
-    
+#fermeture, puis dilatation
+  
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
     dilated = cv2.dilate(image, kernel, iterations = 1)
-    plt.imshow(dilated,'gray')
-    plt.show()
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-    eroded = cv2.erode(dilated, kernel, iterations = 2)
-
-    plt.imshow(eroded,'gray')
-    plt.show()
-    
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (6,6))
-    dilated = cv2.dilate(eroded, kernel, iterations = 2)
-    
-    contour = cv2.Canny(dilated, 50, 200)
-    
-    plt.imshow(dilated,'gray')
-    plt.show()
-    cv2.imshow("Image", contour)
+    cv2.imshow("Image", dilated)
     cv2.waitKey(0)
+    
+#    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+#    eroded = cv2.erode(dilated, kernel, iterations = 2)
+#
+#    cv2.imshow("Image", eroded)
+#    cv2.waitKey(0)
+#    
+#    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (6,6))
+#    dilated = cv2.dilate(eroded, kernel, iterations = 2)
+#    
+#    cv2.imshow("Image", dilated)
+#    cv2.waitKey(0)
     
     return dilated
     
-def findLogo(dilated):
-    tdil=np.transpose(dilated)
-    nbBlanc=sum(sum(tdil==255))
-    freqBlanc=nbBlanc/(dilated.shape[0]*dilated.shape[1])
-    densityL=sum(tdil==255)/tdil.shape[0]
-    print(densityL)
-    densityC=sum(dilated==255)/dilated.shape[0]
-    zoneL=[]
-    zonestart=-1
-    for i in range(len(densityL)-1):
-        if densityL[i]!=0:
-            if zonestart==-1 :
-                zonestart=i
-            
-        elif zonestart!=-1:
-            zoneL.append([zonestart,i])
-            zonestart=-1
-        
-    if zonestart!=-1:
-        zoneL.append([zonestart,len(densityL)-1])
-    print(zoneL)
-    
-    zoneC=[]
-    zonestart=-1
-    for i in range(len(densityC)-1):
-        if densityC[i]!=0:
-            if zonestart==-1 :
-                zonestart=i
-            
-        elif zonestart!=-1:
-            zoneC.append([zonestart,i])
-            zonestart=-1
-        
-    if zonestart!=-1:
-        zoneC.append([zonestart,len(densityC)-1])
-    print(zoneC)
 
-    return zoneL,zoneC
 
-def recentre (pict):
+def recentre (zone):
+#recentre la zone sur la partie pouvant être le logo   
     
-    plt.imshow(pict,'gray')
-    plt.show()
-    tpict=np.transpose(pict)
+    tpict=np.transpose(zone)
     
     densityL=sum(tpict==0)
 
-    densityC=sum(pict==0)
+    densityC=sum(zone==0)
     
     startC=0
     endC=-1
@@ -192,7 +157,7 @@ def isRed(img):
     return fRed>0.9
     
 def extendArea(pict,startX,startY,endX,endY):
-    
+# agrandit la zone pour matcher les partie du logo pas prises en compte
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (4,4))
     pict = cv2.dilate(pict, kernel, iterations = 2)
     
@@ -216,6 +181,7 @@ def extendArea(pict,startX,startY,endX,endY):
     
 
 def MethodeMoy(dilated):
+# délimite les zones en utilisant la moyenne comme valeur de coupure
     tdil=np.transpose(dilated)
     densityL=sum(tdil==255)/tdil.shape[0]
     densityC=sum(dilated==255)/dilated.shape[0]
@@ -264,7 +230,7 @@ def MethodeMoy(dilated):
     
 def getOutside(zones):
     #TODO : améliorer cette méthode
-
+# réunis les zones qui s'intersectent en une grande zone
     for partie in range(len(zones)) : 
             for k in range(len(zones)) :
                 if k!=partie:
@@ -293,97 +259,113 @@ def isInside(x1,y1, x2,y2, xx, yy):
         print(str(x1) + " "+ str(y1) + " " + str(x2) + " " + str(y2)+ " " + str(xx)+ " " + str(yy) )
         return True
     else: return False
+    
+    
+    
+    
+    
+    
+#########################################################################################
+# début du code 
+#########################################################################################
+    
+#chargement de l'image
+image = cv2.imread("..\\Exemple-annonces\\oo21.jpg")
 
-
-image = cv2.imread("..\\Exemple-annonces\\oo2.jpg")
+# convertion en HSV
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+# Isolation du rouge en HSV
 lower = np.array([120, 80, 80]) 
 upper = np.array([179, 255, 255]) 
 red = cv2.inRange(hsv, lower, upper)
+
+#rajout du rouge en RGB (BGR en openCV)
 lower = np.array([0, 80, 80]) 
 upper = np.array([5, 255, 255]) 
 red += cv2.inRange(hsv, lower, upper)
+
+#Calcul de la propostion de rouge dans l'image
 nbRed=sum(sum(red ==255))
 freqRed=nbRed/(red.shape[1]*red.shape[0])
 print(freqRed)
 
 if freqRed >0.15 :
     if freqRed>0.8:
-        print("Care, background Red")
+        print("Care, Red background ")
     else :
         print("Red computer")
-    #réduit au capot de l'ordi (rotation pour récupérer bien)
+        
+    #réduit au capot de l'ordi (rotation pour récupérer la meilleur zone)
     startX,startY,endX,endY, angle = findCapot(red,freqRed)
     
-    #réduit image normale
     image= rotateImage(image,angle)
     img = image[startY:endY,startX:endX]
     im=img.copy() # copie 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bilateralFilter(gray, 11, 17, 17)
-    contours = cv2.Canny(gray, 30, 200)
-    cv2.imshow("Image", contours)
-    cv2.waitKey(0)
-    
-    #réduit le red
-    red = rotateImage(red,angle)
-    red= red[startY:endY,startX:endX]
-    
-    #traitement homogénéisation lumière
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    clahe = cv2.createCLAHE(clipLimit=8, tileGridSize=(20,20))
-    gray = clahe.apply(gray)
-    cv2.fastNlMeansDenoising(gray,gray,30,41,61)
-    cv2.imshow("Image", gray)
-    cv2.waitKey(0)
-    
-    
-    edged = cv2.bilateralFilter(gray, 11, 17, 17)
-    edged = cv2.Canny(edged, 30, 200)
-    cv2.imshow("Image", edged)
-    cv2.waitKey(0)
-    
-    #binarisation
-    _,binaire = cv2.threshold(gray,180,255,cv2.THRESH_BINARY)
-    plt.imshow(binaire,'gray')
-    plt.show()
     
     #affiche ou est le capot
     cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
     cv2.imshow("Image", image)
     cv2.waitKey(0)
     
-    # TODO: enlever le bruit issue des bords trop clairs 
-    if sum(binaire[0,:]==0)>3:
-        print("premire ligne")
-    if sum(binaire[-1,:]==0)>3:
-        print("derniere ligne")
-    if sum(binaire[:,0]==0)>3:
-        print("premire colone")
-    if sum(binaire[:,-1]==0)>3:
-        print("derniere colone")
+    #récupération des contours
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+    contours = cv2.Canny(gray, 30, 200)
+    cv2.imshow("Contours", contours)
+    cv2.waitKey(0)
+    
+    #rotation et centrage sur l'image avec rouge
+    red = rotateImage(red,angle)
+    red= red[startY:endY,startX:endX]
+    
+    #traitement homogénéisation lumière
+#    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#    clahe = cv2.createCLAHE(clipLimit=8, tileGridSize=(20,20))
+#    gray = clahe.apply(gray)
+#    cv2.fastNlMeansDenoising(gray,gray,30,41,61)
+#    cv2.imshow("Image", gray)
+#    cv2.waitKey(0)
+#    
+#    
+#    edged = cv2.bilateralFilter(gray, 11, 17, 17)
+#    edged = cv2.Canny(edged, 30, 200)
+#    cv2.imshow("Contour2", edged)
+#    cv2.waitKey(0)
+    
+    #binarisation
+#    _,binaire = cv2.threshold(gray,180,255,cv2.THRESH_BINARY)
+#    plt.imshow(binaire,'gray')
+#    plt.show()
+    
         
-    dilated = locateLogo(edged)
-    MethodeMoy(dilated)
-    zoneL,zoneC=findLogo(dilated)
+#----------Recherche des zones    
+    # fermeture + dilatation sur les contours
+    dilated = locateLogo(contours)
+    
+    #délimitation des zones à partir de la moyenne
     zoneLb,zoneCb=MethodeMoy(dilated)
 
     zones=[]
     for l in zoneLb:
         for c in zoneCb:
             propBlancs = sum(sum(dilated[l[0]:l[1],c[0]:c[1]]))
-            if propBlancs>0.5:
-                if isRed(red[l[0]:l[1],c[0]:c[1]])==False:
+            if propBlancs>0.5: #pas de zone fantome
+                if isRed(red[l[0]:l[1],c[0]:c[1]])==False: #pas de zone toute rouge
+                    
+                    #dessine rectangle vert
                     cv2.rectangle(img, (c[0], l[0]), (c[1], l[1]), (0, 255, 0), 2)
                     
                     #recentre la zone sur le logo (à partir de l'image rouge)
                     startL,endL,startC,endC = recentre(red[l[0]:l[1],c[0]:c[1]])
+                    
+                    #dessine Bleu
                     cv2.rectangle(img, (c[0]+startC, l[0]+startL), (c[1]+endC, l[1]+endL), (255, 0, 0), 2)
                     
                     #agrandi la zone si on trouve toujours des contours autour
-                    
                     startX,endX,startY,endY = extendArea(contours,c[0]+startC , l[0]+startL , c[1]+endC ,l[1]+endL)
+                    
+                    #dessine Blanc                    
                     cv2.rectangle(img, (startX, startY), (endX, endY), (255, 255, 255), 2)
                     if  endX - startX >10 and endY - startY >10:
                         zones.append([startX,endX,startY,endY])
@@ -391,36 +373,39 @@ if freqRed >0.15 :
     cv2.imshow("Image", img)
     cv2.waitKey(0)
     
-    print("*"*100)
-    print(zones)
+    #netttoie toutes les zones obtenues
     zones = getOutside(zones)
-    print("*"*100)
-    print(zones)
+ 
     
-
+    img = im.copy()
     for z in zones :
-        cv2.rectangle(im, (z[0], z[2]), (z[1], z[3]), (255, 255, 255), 2) 
-    cv2.imshow("Image", im)
+        cv2.rectangle(img, (z[0], z[2]), (z[1], z[3]), (255, 255, 255), 2) 
+    cv2.imshow("Image", img)
     cv2.waitKey(0)
     
-    #logoreco 
     
-    template = cv2.imread("..\\Exemple-annonces\\template5.jpg")
-    template= cv2.flip(template, -1)
+#---------- reconnaissance du logo    
+    #chargement du template
+    template = cv2.imread("..\\Exemple-annonces\\t5.jpg")
+    
+    # contours du template
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
     template = cv2.Canny(template, 50, 200)
+    cv2.imshow("Image", template)
+    cv2.waitKey(0)
     (tH, tW) = template.shape[:2]
     
     for z in zones:
         logo = im[z[2]:z[3],z[0]:z[1]]
         logo = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
-        plt.imshow(logo,'gray')
-        plt.show()
+    
         found = None
-        for rotangle in range(0,360,20):
+        
+        # recherche le logo dans l'image
+        for rotangle in range(-5,5,50):
             grayt = rotateImage(logo,rotangle)
         # loop over the scales of the image
-            for scale in np.linspace(0.5, 3, 50)[::-1]:
+            for scale in np.linspace(0.3, 3, 200)[::-1]:
                 # resize the image according to the scale, and keep track
                 # of the ratio of the resizing
                 resized = imutils.resize(grayt, width = int(grayt.shape[1] * scale))
@@ -447,39 +432,12 @@ if freqRed >0.15 :
         (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
         (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
         print(found[0])
-        if found[0]>0.25:
-            print('-'*100 + '\n' + '*'*100)
-            print("TROUVéééééééééééééééééééééééééééééé")
-            print('-'*100 + '\n' + '*'*100)
+        
         # draw a bounding box around the detected result and display the image
         image=rotateImage(logo,found[3])
         cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-        cv2.imshow("Image", image)
-        cv2.waitKey(0)
-#    logoPosition={}
-#    pos=0
-#    for l in zoneL:
-#        for c in zoneC:
-#            propBlancs = sum(sum(dilated[l[0]:l[1],c[0]:c[1]]))
-#            if propBlancs>0.5:
-#                if isRed(red[l[0]:l[1],c[0]:c[1]])==False:
-#                    cv2.rectangle(img, (c[0], l[0]), (c[1], l[1]), (0, 255, 0), 2)
-#                    
-#                    #recentre la zone sur le logo (à partir de l'image rouge)
-#                    startL,endL,startC,endC = recentre(red[l[0]:l[1],c[0]:c[1]])
-#                    cv2.rectangle(img, (c[0]+startC, l[0]+startL), (c[1]+endC, l[1]+endL), (255, 0, 0), 2)
-#                    
-#                    #agrandi la zone si on trouve toujours des zones non rouges autour
-#                    startX,endX,startY,endY = extendArea(red,c[0]+startC , l[0]+startL , c[1]+endC ,l[1]+endL)
-#                    
-#                    #TODO : si c'est au bord ca dégage (marche pas tous le temps)
-#                    if startX==0 or startY==0 or endX==red.shape[0]-1 or endY==red.shape[1]-1:
-#                        print("AU BORD DONC DEGAGE")
-#                    else :
-#                        logoPosition[pos]=[startX,startY,endX,endY]
-#                        cv2.rectangle(img, (startX, startY), (endX, endY), (255, 255, 255), 2)
-#    cv2.imshow("Image", img)
-#    cv2.waitKey(0)
+        plt.imshow(image,'gray')
+        plt.show()
     
 elif freqRed>0.05 :
     print("Some Red")
